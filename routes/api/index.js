ec0381c3cd629e223a84af9b42a9a3f5a949181a
Ferny <fernc28@outlook.com>
2023-05-01 23:49:07 -0400

    working on controllers

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,6 +3,7 @@ const userRoutes = require("./user_routes")
 const thoughtRoutes = require('./thoughs_routes');
 
 router.use("/users", userRoutes)
+router.use("/thoughts", thoughtRoutes)
 
 
 module.exports = router;
